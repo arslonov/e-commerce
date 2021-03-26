827ce3c1f9dc67f39b9df65f939c63f6303207f6
arslonov <rajabovsardor050@gmail.com>
2021-03-26 22:01:02 +0500

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,5 @@
+<footer style="margin-top: 200px">
+    <div class="footer-copyright text-center py-3">© 2021 Copyright:
+        <a href=""> E-commerce.com</a>
+    </div>
+</footer>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="">E-Commerce</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Orders</a>
+            </li>
+        </ul>
+        <span class="navbar-text">
+     cart(0)
+    </span>
+    </div>
+</nav>
